188c2e5c769c85be6f2946e362b79d83f26274d3
Ismail <ismailrzouki7@gmail.com>
2020-06-21 13:34:54 +0200

    Setup the Project

---
+++ src/main/java/com/codewars/kyu/Drink.java
@@ -0,0 +1,21 @@
+package com.codewars.kyu;
+
+import java.util.regex.*;
+
+public class Drink {
+
+	public static String hydrate(String drinkString) {
+		Pattern pattern = Pattern.compile("(\\d+)");
+		Matcher matcher = null;
+		int sum = 0;
+		String[] drinks = drinkString.split(",");
+		for (int i = 0; i < drinks.length; i++) {
+			matcher = pattern.matcher(drinks[i].trim());
+			while(matcher.find()) {
+				sum += Integer.valueOf(matcher.group(1));
+			}
+		}
+		return sum > 1 ? String.format("%d glasses of water", sum) : String.format("%d glass of water", sum);
+	}
+
+}
---
+++ src/main/java/com/codewars/kyu/Kyu.java
@@ -0,0 +1,9 @@
+package com.codewars.kyu;
+
+public class Kyu {
+
+	public static void main(String[] args) {
+		System.out.println(Drink.hydrate("1 shot, 5 beers, 2 shots, 1 glass of wine, 1 beer"));
+	}
+
+}
